128a0b2a32bca552fb955cf8b20fea39f9d9efdf
Dan Fairburn <dan@danfairburn.com>
2023-03-30 01:16:23 +0100

    clear up constant definitions and create default path builders

--- cmd/use.go
+++ cmd/use.go
@@ -3,12 +3,20 @@ package cmd
 import (
 	"errors"
 
+	"github.com/dfairburn/tp/config"
 	"github.com/dfairburn/tp/handlers"
 	"github.com/spf13/cobra"
 )
 
 var (
 	// local flags
+	// TODO: --help show all available overrides
+	overrides     []string
+	overrideUsage = "A comma separated list of variable overrides.\n" +
+		"A variable override is a list of strings in the following format:\n" +
+		"The of the name of the variable, followed by a semicolon, followed by the override value, " +
+		"e.g:\n" +
+		"--overrides=variableName1:overriddenValue1,variableName2:overriddenValue2"
 	useCmd = &cobra.Command{
 		Use:   "use",
 		Short: "Uses a given template to send a curl request",
@@ -18,12 +26,28 @@ var (
 			if len(args) != 1 {
 				return errors.New("no path provided")
 			}
+			var vars map[interface{}]interface{}
 
-			return handlers.Use(logger, c, varsFile, args[0])
+			if c.VariableDefinitionFile == "" {
+				// if variableDefinitionFile isn't set in config, don't add it to paths to be checked
+				vars = config.LoadVars(logger, varsFile)
+			} else {
+				vars = config.LoadVars(logger, varsFile, c.VariableDefinitionFile)
+			}
+			overrides := validateOverrides(overrides)
+			logger.Info("overrides: ", overrides)
+
+			return handlers.Use(logger, args[0], vars)
 		},
 	}
 )
 
 func init() {
+	useCmd.Flags().StringArrayP("overrides", "o", overrides, overrideUsage)
+
 	rootCmd.AddCommand(useCmd)
+}
+
+func validateOverrides(o []string) []string {
+	return o
 }
--- config/vars.go
+++ config/vars.go
@@ -1,33 +1,18 @@
 package config
 
 import (
-	"gopkg.in/yaml.v3"
 	"io"
 
+	"gopkg.in/yaml.v3"
+
 	logging "github.com/sirupsen/logrus"
-)
-
-const (
-	VarHomeLocInDirFile = HomeLoc + VarDir + VarFilename
-	VarHomeLocFile      = HomeLoc + VarFilename
-	VarRelLocInDirFile  = RelLoc + VarDir + VarFilename
-	VarRelLocFile       = RelLoc + VarFilename
-)
-
-var (
-	varPathsToCheck = []string{
-		VarHomeLocInDirFile,
-		VarHomeLocFile,
-		VarRelLocInDirFile,
-		VarRelLocFile,
-	}
 )
 
 func LoadVars(logger *logging.Logger, paths ...string) map[interface{}]interface{} {
 	y := make(map[interface{}]interface{})
 
 	// this gives precedent to paths passed in via config and flags, then processes the default file paths
-	paths = append(paths, varPathsToCheck...)
+	paths = append(paths, buildVarPaths().paths...)
 
 	f, err := tryFiles(logger, paths...)
 	if err != nil {

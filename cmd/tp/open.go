43f13a2a8e9af5c4f6463d3683b02421acac8b08
Dan Fairburn <dan.fairburn@siemens.com>
2024-04-26 11:23:37 +0100

    create new files on open and load into default text editor

---
+++ cmd/tp/open.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"github.com/dfairburn/tp/handlers"
+	"github.com/spf13/cobra"
+)
+
+var (
+	// local flags
+	newCmd = &cobra.Command{
+		Use:   "open",
+		Short: "Open creates a new template or opens an existing one",
+		Long:  "Open creates a new template, or opens an existing one and loads it into your configured editor (configured by $EDITOR, default vim)",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return errors.New("need new path to be returned for new template")
+			}
+
+			filename := args[0]
+			return handlers.New(logger, c.TemplatesDirectoryPath, filename)
+		},
+	}
+)
+
+func init() {
+	newCmd.Flags().StringSliceVarP(&overrides, "overrides", "o", []string{}, overrideUsage)
+
+	rootCmd.AddCommand(newCmd)
+}
--- handlers/new.go
+++ handlers/new.go
@@ -2,21 +2,74 @@ package handlers
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	logging "github.com/sirupsen/logrus"
+
+	tppaths "github.com/dfairburn/tp/paths"
 )
 
-func New(logger *logging.Logger, template string) error {
-	if _, err := os.Stat(template); errors.Is(err, os.ErrNotExist) {
-		_, err := os.Create(template)
+const (
+	editor       = "EDITOR"
+	baseTemplate = `===Url
+
+===Method
+
+===Headers
+
+===Body`
+)
+
+func New(logger *logging.Logger, templateDir, template string) error {
+	expanded := tppaths.Expand(templateDir)
+	d, err := os.Stat(expanded)
+	if err != nil {
+		return err
+	}
+
+	if !d.IsDir() {
+		return fmt.Errorf("configured templates dir %s is not a directory", expanded)
+	}
+
+	path := filepath.Join(expanded, template)
+	dir, file := filepath.Split(path)
+	err = os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+
+	if filepath.Ext(file) != ".tmpl" || filepath.Ext(file) == "" {
+		path = path + ".tmpl"
+	}
+
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		err = os.WriteFile(path, []byte(baseTemplate), 0644)
 		if err != nil {
 			return err
 		}
+	} else {
+		ed := os.Getenv(editor)
+		if ed == "" {
+			ed = "vim"
+		}
 
-		//f.WriteString()
-
-		// path/to/whatever does not exist
+		return runEditor(ed, path)
 	}
-	return nil
+
+	ed := os.Getenv(editor)
+	if ed == "" {
+		ed = "vim"
+	}
+
+	return runEditor(ed, path)
+}
+
+func runEditor(ed string, path string) error {
+	cmd := exec.Command(ed, path)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
 }

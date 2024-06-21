a9e53b871ab83b10fc085d964644b35d941fa997
Dan Fairburn <dan.fairburn@siemens.com>
2024-06-21 15:06:37 +0100

    list out all templates

---
+++ cmd/tp/list.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/dfairburn/tp/config"
+	"github.com/spf13/cobra"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var (
+	// local flags
+	listCmd = &cobra.Command{
+		Use:   "list",
+		Short: "List returns all the templates in the configured templates directory",
+		Long:  "List returns all the templates in the configured templates directory",
+		Args:  cobra.MinimumNArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			re, err := regexp.Compile(".*\\.tmpl$")
+			if err != nil {
+				return err
+			}
+
+			var templates []string
+			walkFunc := func(p string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if ok := re.Match([]byte(p)); !ok {
+					logger.Errorf("path %v does not have .tmpl extension\n", p)
+					return nil
+				}
+				if info.IsDir() {
+					logger.Errorf("path %v is a directory\n", p)
+					return nil
+				}
+
+				templates = append(templates, p)
+				return nil
+			}
+			err = config.LoadTemplateFiles(logger, c.TemplatesDirectoryPath, walkFunc)
+			if err != nil {
+				logger.Fatalf("cannot find templates in templates dir %v, error: %v", c.TemplatesDirectoryPath, err)
+			}
+
+			_, err = io.WriteString(os.Stdout, strings.Join(templates, "\n"))
+			_, _ = io.WriteString(os.Stdout, "\n")
+			return err
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}

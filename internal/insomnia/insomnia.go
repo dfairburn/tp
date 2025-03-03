0a1f149ebe8c6b7829fc2dfd67290a2beb6b7638
Dan Fairburn <dan.fairburn@siemens.com>
2025-03-03 17:29:59 +0000

    start parsing out the export doc into json

--- internal/insomnia/insomnia.go
+++ internal/insomnia/insomnia.go
@@ -20,5 +20,26 @@ func ParseInsomniaExport(file []byte) (*Collection, error) {
 
 	fmt.Printf("export: %+v\n", string(b))
 
+	for _, resource := range export.Resources {
+
+		if resource.Type == "environment" {
+			// TODO: convert env json into yaml (probably a lib that can do this)
+			fmt.Println("=========================")
+			fmt.Printf("type: %s\n", resource.Type)
+			fmt.Printf("data: %s\n", resource.Data)
+			fmt.Println("=========================")
+		} else if resource.Type == "request" {
+			fmt.Println("=========================")
+			fmt.Printf("url: %s\n", resource.Url)
+			fmt.Printf("method: %s\n", resource.Url)
+			fmt.Printf("headers: %s\n", resource.Headers)
+			fmt.Println("=========================")
+		} else if resource.Type == "request_group" {
+			fmt.Println("=========================")
+			fmt.Printf("service name: %s\n", resource.Name)
+			fmt.Println("=========================")
+		}
+	}
+
 	return nil, nil
 }
